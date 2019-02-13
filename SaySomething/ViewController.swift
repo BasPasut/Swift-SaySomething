ee631a0f94d90aa8845b9cb534b94b059824426a
Pasut Kittiprapas <baspasut@Pasuts-Mac.local>
2019-02-13 08:27:23 -0800

    :apple: first commit

--- SaySomething/ViewController.swift
+++ SaySomething/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    @IBAction func button(_ sender: UIButton) {
+        textLabel.text = textField.text
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
